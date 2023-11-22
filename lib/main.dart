0e060b625a2464cb9ce7729559ef5e24a81df15e
zvolmar <volmarzach@yahoo.com>
2023-11-22 13:44:16 -0500

    Janitor duty

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:speech_to_text/speech_to_text.dart';
+import 'package:flutter_tts/flutter_tts.dart';
+import 'updated_database.dart' as phone_specs;
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-        textTheme:
-            TextTheme(headline2: TextStyle(color: Colors.lightGreenAccent))
-                .apply(
-                    bodyColor: Colors.lightGreenAccent,
-                    displayColor: Colors.lightGreenAccent),
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
-        useMaterial3: true,
+        primarySwatch: Colors.green,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
@@ -27,57 +24,244 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
   final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  MyHomePageState createState() => MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+//********************************************
+//****************App Logic*******************
+//********************************************
+class MyHomePageState extends State<MyHomePage> {
+  late SpeechToText _speech;
+  FlutterTts flutterTts = FlutterTts();
+  bool _isListening = false;
+  String _recognizedSpeech = '';
+  String _displayedAnswer = '';
+  String _spokenAnswer = '';
 
-  void _incrementCounter() {
+  @override
+  void initState() {
+    super.initState();
+    _speech = SpeechToText();
+    _initializeSpeech();
+  }
+
+  void _initializeSpeech() async {
+    bool available = await _speech.initialize(
+      onError: (error) => debugPrint('Error: $error'),
+      onStatus: (status) => debugPrint('Status: $status'),
+    );
+    if (!available) {
+      debugPrint('Speech recognition not available');
+    }
+  }
+
+  void _toggleListening() {
+    if (!_isListening) {
+      _startListening();
+    } else {
+      _stopListening();
+    }
     setState(() {
-      _counter++;
+      _isListening = !_isListening;
     });
   }
 
+  void _startListening() async {
+    if (!_speech.isAvailable || !_speech.isListening) {
+      bool available = await _speech.initialize(
+        onError: (error) => debugPrint('Error: $error'),
+        onStatus: (status) => debugPrint('Status: $status'),
+      );
+      if (!available) {
+        debugPrint('Speech recognition not available');
+        return;
+      }
+    }
+
+    if (!_speech.isListening) {
+      await _speech.listen(
+        onResult: (result) {
+          if (result.finalResult) {
+            setState(() {
+              _recognizedSpeech = result.recognizedWords;
+            });
+            _displayAnswer(
+                _recognizedSpeech); // Display the answer for recognized text
+          } else {
+            setState(() {
+              _recognizedSpeech = result.recognizedWords;
+            });
+          }
+        },
+      );
+    }
+  }
+
+  void _stopListening() {
+    if (_speech.isListening) {
+      _speech.stop();
+    }
+  }
+
+  Future<void> _speak(String text) async {
+    await flutterTts.setLanguage('en-US');
+
+    // Setting voice parameters for natural sound
+    await flutterTts.setPitch(1.0); // 1.0 is the normal pitch
+    await flutterTts.setSpeechRate(1.0); // 1.0 is the normal speech rate
+    await flutterTts.setVolume(1.0); // 1.0 is the maximum volume
+
+    await flutterTts.speak(text);
+  }
+
+  void _displayAnswer(String recognizedSpeech) {
+    // Implementing logic to find an answer based on a keyword in the recognized speech
+
+    String foundAnswer = "Answer not found"; //Default value
+    String foundDisplayAnswer = "Answer not found"; //Default value
+    //The spoken database removes acronyms from the bot's speech patterns, this way it will
+    //display things like "mp" but the bot will still say "megapixel"
+    phone_specs.spokenResponse.forEach((question, answer) {
+      if (recognizedSpeech.toLowerCase().contains(question.toLowerCase())) {
+        foundAnswer = answer;
+        return;
+      }
+    });
+
+    phone_specs.displayedResponse.forEach((question, answer) {
+      if (recognizedSpeech.toLowerCase().contains(question.toLowerCase())) {
+        foundDisplayAnswer = answer;
+        return;
+      }
+    });
+
+    setState(() {
+      _displayedAnswer = foundDisplayAnswer; // Set the displayed answer
+      _spokenAnswer = foundAnswer; // Set the answer to be spoken
+    });
+
+    _speak(_spokenAnswer); // Speak the found answer
+  }
+
+//*************************************************
+//*******************Styling***********************
+//*************************************************
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
         decoration: const BoxDecoration(
-            image: DecorationImage(
-                image: AssetImage("assets/background.jpg"), fit: BoxFit.cover)),
+          image: DecorationImage(
+            image: AssetImage("assets/background.jpg"),
+            fit: BoxFit.cover,
+          ),
+        ),
         child: Container(
-          margin: EdgeInsets.all(50),
-          decoration: BoxDecoration(
-              border: Border.all(color: Colors.lightGreenAccent),
-              borderRadius: BorderRadius.circular(30)),
+          margin: const EdgeInsets.all(50),
           child: Row(
             mainAxisAlignment: MainAxisAlignment.start,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
               Container(
-                  height: 700,
-                  width: 600,
-                  margin: EdgeInsets.all(0),
-                  padding: EdgeInsets.all(0),
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(30),
-                      color: Colors.lightGreenAccent),
+                height: 700,
+                width: 600,
+                margin: EdgeInsets.zero,
+                padding: EdgeInsets.zero,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(30),
+                  color: Colors.amber,
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Image.asset("assets/zfold5.png", fit: BoxFit.contain),
+                    Container(
+                      margin: const EdgeInsets.only(top: 25),
+                      child: const Text('Samsung Galaxy Z Fold 5'),
+                    ),
+                  ],
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  padding: const EdgeInsets.all(20),
                   child: Column(
                     mainAxisAlignment: MainAxisAlignment.center,
                     crossAxisAlignment: CrossAxisAlignment.center,
                     children: [
-                      Image.asset("assets/zfold5.jpg", fit: BoxFit.contain),
-                      Container(
-                          margin: EdgeInsets.only(top: 25),
-                          child: const Text('Samsung Galazy Z Fold 5'))
+                      ElevatedButton(
+                        onPressed: _toggleListening,
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor:
+                              _isListening ? Colors.red : Colors.amber,
+                        ),
+                        child: Text(
+                          _isListening ? 'Stop Listening' : 'Start Listening',
+                          style: const TextStyle(color: Colors.white),
+                        ),
+                      ),
+                      const SizedBox(height: 20),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Expanded(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                const Text(
+                                  'Recognized Speech:',
+                                  style: TextStyle(
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                                Text(
+                                  _recognizedSpeech,
+                                  style: const TextStyle(
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                          Expanded(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                const Text(
+                                  'Displayed Answer:',
+                                  style: TextStyle(
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                                Text(
+                                  _displayedAnswer,
+                                  style: const TextStyle(
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
                     ],
-                  )),
-              questionList
+                  ),
+                ),
+              ),
             ],
           ),
         ),
@@ -85,33 +269,3 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 }
-
-const listTextStyle = TextStyle(
-  color: Colors.lightGreenAccent,
-  fontWeight: FontWeight.w800,
-  fontFamily: 'Roboto',
-  letterSpacing: 0.5,
-  fontSize: 18,
-  height: 2,
-);
-
-final questionList = DefaultTextStyle.merge(
-    style: listTextStyle,
-    child: Container(
-      margin: EdgeInsets.all(50),
-      padding: EdgeInsets.all(20),
-      width: 450,
-      decoration: BoxDecoration(
-        border: Border.all(color: Colors.lightGreenAccent),
-      ),
-      child: const Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            Text(
-                'To ask a question, press the mute button on the side of the headset'),
-            Text('data'),
-            Text('data'),
-            Text('data')
-          ]),
-    ));
---
+++ lib/updated_database.dart
@@ -0,0 +1,55 @@
+final Map<String, String> displayedResponse = {
+  "charging": "USB Type C. 25W wired, 15W wireless, 4.5W reverse wireless",
+  "processor": "Snapdragon 8 Gen 2",
+  "water resistance": "Yes, IPX8 water resistant up to 1.5m for 30 minutes",
+  "dimensions":
+      "Unfolded: 154.9mm x 129.9mm x 6.1mm, Folded: 154.9mm x 67.1mm x 13.4mm",
+  "color": "Icy Blue, Phantom Black, Cream, Gray, Blue",
+  "glass":
+      "Gorilla glass Victus 2 for folded front and back. It supports a plastic front shield when unfolded",
+  "sim card": "Nano-SIM or eSIM support. It does support dual SIM",
+  "s pen": "No, but does support the use of an S pen sold separately",
+  "weight": "253g or 8.92oz",
+  "512 GB": "\$1920, \$120 more than the base model",
+  "base model": "\$1800 256GB",
+  "storage": "256GB, 512GB, 1TB",
+  "video":
+      "Main Camera: 8K 30fps, 4K 60fps, 1080p 60/240fps. Selfie Camera: 4K 30fps, 1080p 30/60fps",
+  "battery": "4400mAh",
+  "operating system": "Android 13",
+  "camera":
+      "Main Camera: 50mp wide angle, 10mp telephoto, 12mp ultrawide. Selfie Camera: 4 mp wide angle, 10 mp wide angle",
+  "release": "August 11, 2023",
+  "display": "7.6in foldable AMOLED 120 hz display",
+  "ram": "12GB of RAM",
+  "1 TB": "\$2160, \$360 more than the base model"
+};
+
+final Map<String, String> spokenResponse = {
+  "charging":
+      "USB Type C. 25 watt wired, 15 watt wireless, 4.5 watt reverse wireless",
+  "processor": "Snapdragon 8 Gen 2",
+  "water resistance":
+      "Yes, IPX8 water resistant up to 1.5 meter for 30 minutes",
+  "dimensions":
+      "Unfolded: 154.9 by 129.9 by 6.1 millimeter, Folded: 154.9 by 67.1 by 13.4 millimeter",
+  "color": "Icy Blue, Phantom Black, Cream, Gray, Blue",
+  "glass":
+      "Gorilla glass Victus 2 for folded front and back. It supports a plastic front shield when unfolded",
+  "sim card": "Nano-SIM or eSIM support. It does support dual SIM",
+  "s pen": "No, but does support the use of an S pen sold separately",
+  "weight": "253 grams or 8.92 ounces",
+  "512 GB": "\$1920, \$120 more than the base model",
+  "base model": "\$1800 256GB",
+  "storage": "256GB, 512GB, 1TB",
+  "video":
+      "Main Camera: 8K 30 frames per second, 4K 60 frames per second, 1080p 60/240 frames per second. Selfie Camera: 4K 30fps, 1080p 30/60fps",
+  "battery": "4400 milliamp per hour",
+  "operating system": "Android 13",
+  "camera":
+      "Main Camera: 50 megapixel wide angle, 10 megapixel telephoto, 12 megapixel ultrawide. Selfie Camera: 4 megapixel wide angle, 10 megapixel wide angle",
+  "release": "August 11, 2023",
+  "display": "7.6 inch foldable AMOLED 120 hertz display",
+  "ram": "12 gigabytes of RAM",
+  "1 TB": "\$2160, \$360 more than the base model"
+};
